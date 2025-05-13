c830c72b36fd61c86885aa1a508101b163bc1846
Joakimglodedata <joakim.glodedata@gmail.com>
2025-05-13 14:23:46 +0200

    kuli

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log('Hello!');
+// let baseNumberInput = document.getElementById("baseNumberInput").value; 
+// let baseNumberOutput = document.querySelector("#baseNumberOutput").value
+// let inputNumber = document.querySelector("#inputNumber").value
+// submitBase.onclick = myfunction
+// test.onclick = () => console.log(inputNumber)
+// test1.onclick = () => console.log(baseNumberOutput)
+// test2.onclick = () => console.log(baseNumberInput)
+
+submitOutput.onclick = magic
+
+function magic() {
+    const baseNumberInput = document.getElementById("baseNumberInput").value; 
+    const baseNumberOutput = document.querySelector("#baseNumberOutput").value
+    const inputNumber = document.querySelector("#inputNumber").value
+    const output = document.querySelector("#outputNumberDisplay")
+    console.log(inputNumber)
+    console.log(baseNumberOutput)
+    console.log(baseNumberInput)
+
+    function splitnumber(inputNumber) {
+        return inputNumber
+        .toString()
+        .split("")
+        .map(Number);
+    }
+    
+    const splitInput = splitnumber(inputNumber)
+    document.querySelector("#splitDisplay").textContent = `Split result: ${splitInput}`
+
+    
+    //Jobb videre med denne funksjonen
+    const results = splitInput.reverse()
+    .map((value, index) => value * index);
+    console.log(results);
+    
+    
+
+
+    const result = inputNumber * baseNumberInput
+    output.textContent = `Result: ${result}`
+    
+}
+
+
+
+
